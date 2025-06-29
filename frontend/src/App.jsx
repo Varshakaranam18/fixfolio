a41517e0329c572e78b821dc300869d80c7caae7
Varshakaranam18 <varshakaranam18@gmail.com>
2025-06-29 00:22:02 -0400

    added signup page and setted up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import SignUpPage from './Components/SignUpPage';
 import './App.css'
-import LoginPage from './Components/LoginPage'
 
 function App() {
- 
-
   return (
-   <LoginPage />
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
